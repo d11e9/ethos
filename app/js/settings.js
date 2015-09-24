74b477ea729645f9399f7b499ab51d4754a295d1
d11e9 <d11e9@turkd.net>
2015-09-24 15:54:15 +0100

    allow external node_modules for windows build

--- app/js/settings.js
+++ app/js/settings.js
@@ -1,7 +1,7 @@
 var path = require('path')
 var execPath = function (package) {
 	if (process.platform !== 'darwin' && process.execPath.indexOf('node_modules') === -1 )
-		return path.join( process.execPath, '../', package );
+		return path.join( process.execPath, '../node_modules', package );
 	else return package;
 }
 
